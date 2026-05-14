278b9360c037f0016f311c17342070f5a78bf3fe
Fidel <trabajosfidel4@gmail.com>
2026-05-13 23:50:40 -0300

    chore: remove debugging scripts

--- backend/test_static.js
+++
@@ -1,9 +0,0 @@
-async function main() {
-  const url = 'http://localhost:3001/uploads/1778420536680-71685642.jpg';
-  console.log('Fetching static file from:', url);
-  const res = await fetch(url);
-  console.log('Status:', res.status);
-  console.log('Content-Type:', res.headers.get('content-type'));
-}
-
-main().catch(console.error);
